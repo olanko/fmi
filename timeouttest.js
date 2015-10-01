e9b1126f052c1ea8aec3e2a72686f7abfc56946b
Olli Korhonen <olli.korhonen@iki.fi>
2015-10-01 10:26:56 +0300

    testing "wait for X sec or fail"

---
+++ timeouttest.js
@@ -0,0 +1,39 @@
+http = require('http');
+
+function delay(time) {
+  return new Promise(function (fulfill) {
+    setTimeout(fulfill, time);
+  });
+}
+
+function timeout(promise, time) {
+  return Promise.race([promise, delay(time).then(function () {
+    throw new Error('Operation timed out');
+  })]);
+}
+
+function get(url) {
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+        http.get(url, function(res, error) {
+            if (error) {
+                reject(error);
+            }
+
+            resp = "";
+            res.on('data', function(data) {
+                resp += data;
+            });
+            res.on('end', function() {
+                resolve(resp);
+            });
+        });
+    });
+}
+
+timeout(get('http://www.hs.fi'), 1000).then(function(res) {
+    console.log(res);
+}, function(error) {
+    console.log(error);
+});
+
