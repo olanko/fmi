24ed867f0df1ef1bbb25bdaf0abea17ccd22a597
Olli Korhonen <olli.korhonen@iki.fi>
2015-10-23 10:40:12 +0300

    change default dates

--- fetchxml.js
+++ fetchxml.js
@@ -11,7 +11,7 @@ var url = 'http://data.fmi.fi/fmi-apikey/' + APIKEY;
 
 var storedquery_id = 'fmi::observations::weather::daily::simple';
 
-var params = '&place=Kuhmo&starttime=2014-01-01&endtime=2015-01-01';
+var params = '&place=Kuhmo&starttime=2015-09-01'; //&endtime=2015-01-01';
 //var params = '&fmisid=101773';
 
 var query = '/wfs?request=getFeature&storedquery_id=' + storedquery_id + params;
@@ -19,7 +19,7 @@ var query = '/wfs?request=getFeature&storedquery_id=' + storedquery_id + params;
 console.log(url + query);
 
 http.get(url + query, function(res) {
-    var os = fs.createWriteStream('data/response.xml')
+    var os = fs.createWriteStream('data/response.xml');
 
     res.on('data', function(data) {
         os.write(data);
